edfb00a0304039cddf570c9964261c3ad604a185
Thinley <thinleylama44@gmail.com>
2023-09-02 09:55:00 +0530

    Login, register and home page created

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.render("login_register_page.ejs");
+});
+app.get("/register", function (req, res) {
+    res.render("registerPage.ejs");
+});
+app.get("/login", function (req, res) {
+    res.render("loginPage.ejs");
+});
+app.get("/home", function (req, res) {
+    res.render("home.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
